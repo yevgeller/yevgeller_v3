1f7e6f1044f8102d2141df9feb79c3e43e77a8f8
yevgeller@gmail.com <yevgeller@gmail.com>
2022-12-06 13:35:09 -0500

    More links

---
+++ yevgeller_v3/Pages/ProjectIdeas.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace yevgeller_v3.Pages
+{
+    public class ProjectIdeasModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
