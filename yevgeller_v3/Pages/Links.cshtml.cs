632f5f3ded93c29ea0b483a79f0fd455a569e2bf
yevgeller <yevgeller@gmail.com>
2022-12-05 23:13:37 -0500

    Interim commit, UI changes, renaming things

---
+++ yevgeller_v3/Pages/Links.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace yevgeller_v3.Pages
+{
+    public class LinksModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
