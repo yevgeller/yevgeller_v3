c96659644d41781cf93f6c6face07bbb89ce8b99
yevgeller@gmail.com <yevgeller@gmail.com>
2022-12-06 11:21:04 -0500

    Interim commit

---
+++ yevgeller_v3/Pages/LongReads.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace yevgeller_v3.Pages
+{
+    public class LongReadsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
