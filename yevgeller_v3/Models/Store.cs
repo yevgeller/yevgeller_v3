a7ecbf1192f3fda1e432d83b22ce49bea0f0f172
yevgeller <yevgeller@gmail.com>
2022-12-08 22:56:05 -0500

    Interim commit, Links are in the Store now

--- yevgeller_v3/Models/Store.cs
+++ yevgeller_v3/Models/Store.cs
@@ -20,8 +20,15 @@ namespace yevgeller_v3.Models
         public Store()
         {
             articles.Add(new Article("https://yevgellerdesignpatterns.azurewebsites.net/", "My review of Software Design Patterns", "trunk", "Links" ));
+            articles.Add(new Article("https://yevgellerdesignpatterns.azurewebsites.net/Game1", "Mines game v2", "trunk", "Links"));
+
             articles.Add(new Article("https://github.com/rust-lang/rustlings", "Rustlings", "Rust resources", "Links"));
             articles.Add(new Article("https://nostarch.com/rust-programming-language-2nd-edition", "Rust book (No Starch Press)", "Rust resources", "Links"));
+            articles.Add(new Article("https://laracasts.com/topics/vue", "Laracasts", "Vue", "Vue"));
+            articles.Add(new Article("https://exercism.org/", "Exercism", "Exercises", "Exercises"));
+            articles.Add(new Article("https://adventofcode.com/", "Advent of Code", "Exercises", "Exercises"));
+            articles.Add(new Article("https://justjavascript.com/", "Just JavaScript", "JavaScript", "Informational"));
+
             articles.Add(new Article("https://javascript.plainenglish.io/top-javascript-one-liners-will-make-you-a-superhero-af1363083354", "UsefulOne-liners", "JavaScript resources", "Articles"));
             articles.Add(new Article("#", "Design a lift system", "Project Ideas", "Project Ideas"));
             var art = new Article("#", "Design a vending machine -- should accept 1, 5, 10, 25 cents, 1, 2 dollar note", "Project Ideas", "Project Ideas");
