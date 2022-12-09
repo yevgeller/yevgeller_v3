0a55010729ea87cc59b1029a80bc95c3efe207f7
yevgeller <yevgeller@gmail.com>
2022-12-08 21:24:58 -0500

    Interim commit

--- yevgeller_v3/Models/Store.cs
+++ yevgeller_v3/Models/Store.cs
@@ -19,17 +19,19 @@ namespace yevgeller_v3.Models
 
         public Store()
         {
-            //articles
-            //    categories.Add(new Category { Name = ""}
             articles.Add(new Article("https://yevgellerdesignpatterns.azurewebsites.net/", "My review of Software Design Patterns", "trunk", "Links" ));
             articles.Add(new Article("https://github.com/rust-lang/rustlings", "Rustlings", "Rust resources", "Links"));
             articles.Add(new Article("https://nostarch.com/rust-programming-language-2nd-edition", "Rust book (No Starch Press)", "Rust resources", "Links"));
             articles.Add(new Article("https://javascript.plainenglish.io/top-javascript-one-liners-will-make-you-a-superhero-af1363083354", "UsefulOne-liners", "JavaScript resources", "Articles"));
+<<<<<<< HEAD
             articles.Add(new Article("#", "Design a lift system", "Project Ideas", "Project Ideas"));
             var art = new Article("#", "Design a vending machine -- should accept 1, 5, 10, 25 cents, 1, 2 dollar note", "Project Ideas", "Project Ideas");
             art.Comment = "Products: Candy(10), Snack (50), Nuts(90), Coke (25), Pepsi (35), Soda (45)";
             articles.Add(art);
             articles.Add(new Article("#", "Design a traffic controller system for a junction", "Project Ideas", "Project Ideas"));
+=======
+        //copy all other links here
+>>>>>>> Interim commit
         }
 
         public List<Article> GetAllArticles() => articles;
