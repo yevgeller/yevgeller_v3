7949ce459a1012a3cc64ea8e524e6a14325b49f8
yevgeller <yevgeller@gmail.com>
2022-12-09 16:18:44 -0500

    Draft of a display

--- yevgeller_v3/Models/Article.cs
+++ yevgeller_v3/Models/Article.cs
@@ -24,5 +24,21 @@
             CategoryHeader = categoryHeader;
             Category = category;
         }
+
+        public string Control()
+        {
+            if(this.URL != null && this.URL.Length > 0) //anchor tag
+            {
+                return $"<a href={this.URL}>{this.Description}</a>";
+
+            }
+
+            return string.Empty;
+        }
+
+        public string ConrolAsListItem()
+        {
+            return $"<li>{this.Control()}</li>";
+        }
     }
 }
