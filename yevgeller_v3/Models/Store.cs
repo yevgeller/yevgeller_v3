a3df2d0c315a8e769060b3d7bad6753b276d6d11
yevgeller <yevgeller@gmail.com>
2022-12-08 23:03:16 -0500

    Interim commit

--- yevgeller_v3/Models/Store.cs
+++ yevgeller_v3/Models/Store.cs
@@ -19,26 +19,32 @@ namespace yevgeller_v3.Models
 
         public Store()
         {
+            //Links
             articles.Add(new Article("https://yevgellerdesignpatterns.azurewebsites.net/", "My review of Software Design Patterns", "trunk", "Links" ));
             articles.Add(new Article("https://yevgellerdesignpatterns.azurewebsites.net/Game1", "Mines game v2", "trunk", "Links"));
-
             articles.Add(new Article("https://github.com/rust-lang/rustlings", "Rustlings", "Rust resources", "Links"));
             articles.Add(new Article("https://nostarch.com/rust-programming-language-2nd-edition", "Rust book (No Starch Press)", "Rust resources", "Links"));
-            articles.Add(new Article("https://laracasts.com/topics/vue", "Laracasts", "Vue", "Vue"));
+            //Exercises
+            articles.Add(new Article("https://laracasts.com/topics/vue", "Laracasts", "Vue", "Exercises"));
             articles.Add(new Article("https://exercism.org/", "Exercism", "Exercises", "Exercises"));
             articles.Add(new Article("https://adventofcode.com/", "Advent of Code", "Exercises", "Exercises"));
-            articles.Add(new Article("https://justjavascript.com/", "Just JavaScript", "JavaScript", "Informational"));
 
-            articles.Add(new Article("https://javascript.plainenglish.io/top-javascript-one-liners-will-make-you-a-superhero-af1363083354", "UsefulOne-liners", "JavaScript resources", "Articles"));
+            //Articles
+            articles.Add(new Article("https://javascript.plainenglish.io/top-javascript-one-liners-will-make-you-a-superhero-af1363083354", "Useful One-liners", "JavaScript resources", "Articles"));
+            articles.Add(new Article("https://deepu.tech/concurrency-in-modern-languages-js/", "Concurrency in JS", "JavaScript", "Articles"));
+
+            //Project Ideas
             articles.Add(new Article("#", "Design a lift system", "Project Ideas", "Project Ideas"));
-            var art = new Article("#", "Design a vending machine -- should accept 1, 5, 10, 25 cents, 1, 2 dollar note", "Project Ideas", "Project Ideas");
-            art.Comment = "Products: Candy(10), Snack (50), Nuts(90), Coke (25), Pepsi (35), Soda (45)";
-            articles.Add(art);
+            articles.Add(new Article { URL = "#", Description = "Design a vending machine -- should accept 1, 5, 10, 25 cents, 1, 2 dollar note", CategoryHeader = "Project Ideas", Category = "Project Ideas", Comment = "Products: Candy(10), Snack (50), Nuts(90), Coke (25), Pepsi (35), Soda (45)" });
             articles.Add(new Article("#", "Design a traffic controller system for a junction", "Project Ideas", "Project Ideas"));
+            
+            //Old
             articles.Add(new Article("/static/encoder.html", "JS text encoder that prevents machine recognition", "Old", "Old"));
             articles.Add(new Article("/static/jsClock.html", "JS canvas clock", "Old", "Old"));
             articles.Add(new Article("/static/mines.html", "Minesweeper v1", "Old", "Old"));
             articles.Add(new Article("/static/yevgeller_v1.html", "List of old projects", "Old", "Old"));
+            //Random
+            articles.Add(new Article("https://justjavascript.com/", "Just JavaScript", "JavaScript", "Random"));
         }
 
         public List<Article> GetAllArticles() => articles;
