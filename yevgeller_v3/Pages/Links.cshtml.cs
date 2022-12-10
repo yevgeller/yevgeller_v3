1a34543d78cb0cd8b2b69f4add0efc25955963ab
yevgeller <yevgeller@gmail.com>
2022-12-10 17:30:06 -0500

    Update to pages

--- yevgeller_v3/Pages/Links.cshtml.cs
+++ yevgeller_v3/Pages/Links.cshtml.cs
@@ -16,6 +16,7 @@ namespace yevgeller_v3.Pages
         }
 
        internal List<Article> currentSelection = new List<Article>();
+        internal List<string> groupHeaders = new List<string>();
         public void OnGet(string param)
         {
             Param = param;
@@ -29,6 +30,7 @@ namespace yevgeller_v3.Pages
                 currentSelection = s.GetAllArticles();
             }
 
+            groupHeaders = s.GetDistinctGroupHeaders(param);
         }
     }
 }
