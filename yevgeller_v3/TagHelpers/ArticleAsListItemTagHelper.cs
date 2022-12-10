a678ba024840611929c31f9c761f4d7b885e3a60
yevgeller <yevgeller@gmail.com>
2022-12-09 21:15:40 -0500

    Adding a TagHelper to display articles

---
+++ yevgeller_v3/TagHelpers/ArticleAsListItemTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using yevgeller_v3.Models;
+
+namespace yevgeller_v3.TagHelpers
+{
+    public class ArticleAsListItemTagHelper : TagHelper
+    {
+        public Article Article { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "li";    // Replaces <email> with <a> tag
+            output.Attributes.SetAttribute("class", "is-size-5 is-large");
+            output.Content.AppendHtml($@"<a href='{this.Article.URL}'>{this.Article.Description}</a>");
+
+            //output.Content.SetContent(address);
+        }
+    }
+}
