c997be9a0b7d48773a65c093e0f1724670ee04e1
yevgeller <yevgeller@gmail.com>
2025-01-16 20:29:44 -0500

    Got the wrong answer button disabled thing going

--- yevgeller_v3/Pages/Test1.cshtml.cs
+++ yevgeller_v3/Pages/Test1.cshtml.cs
@@ -1,5 +1,3 @@
-using Microsoft.AspNetCore.Http.HttpResults;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using yevgeller_v3.Models.BdpqTestingFramework;
 
@@ -8,49 +6,22 @@ namespace yevgeller_v3.Pages
 {
     public class Test1Model : PageModel
     {
-        //public BdpqTestingFramework repository = new BdpqTestingFramework();
         public readonly IBdpqTestingFramework repo;
         public TestQuestion tq = new();
 
         public Test1Model(IBdpqTestingFramework repository)
         {
             this.repo = repository;
-            tq = new(); // repo.ProcessAnswer(string.Empty);
+            tq = new();
         }
 
         public void OnGet()
         {
             tq = repo.ProcessAnswer(string.Empty);
         }
-
-       // public void OnPostEdit(string answer)
-        //{
-        //    var j = answer;
-        //    var a = 1;
-
-        //    tq = repo.GetNextQuestion();
-        //    //Message = "Edit handler fired";
-        //}
-
-        //public async Task<IActionResult> OnPostProcessAsync(string answer)
         public void OnPostEdit(string answer)
         {
-            //var found = tq.Answers.FirstOrDefault(x=>x.Answer == answer);
-
-           // if (found == null) return NotFound();
-
             tq = repo.ProcessAnswer(answer);
-
-            //return null;
-            //var contact = await _context.Customer.FindAsync(id);
-
-            //if (contact != null)
-            //{
-            //    _context.Customer.Remove(contact);
-            //    await _context.SaveChangesAsync();
-            //}
-
-            //return RedirectToPage();
         }
     }
 }
