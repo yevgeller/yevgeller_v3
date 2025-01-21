10feefaff15d0fccae2e3e2a10251e318d4ace27
yevgeller@gmail.com <yevgeller@gmail.com>
2025-01-20 20:11:31 -0500

    Stats bugfix, cleanup

--- yevgeller_v3/Pages/Test1.cshtml.cs
+++ yevgeller_v3/Pages/Test1.cshtml.cs
@@ -37,53 +37,7 @@ namespace yevgeller_v3.Pages
         {
             if (stats == null) return 0;
             if(stats.Count == 0) return 0;
-            return stats.Sum(x => x.Total);
+            return stats.Sum(x => x.Correct);
         }
     }
 }
-
-/* Other HTML:
- * 
- * 
-
-        <!--
-         <p> @Model.tq.Question</p>
-
-        @foreach (var a in Model.tq.Answers)
-        {
-                     <div>
-                         <input type="radio" id="answer@i" name="SelectedAnswer" value="@a.Answer" />
-                         <label for="answer@i">@a.Answer</label>
-                     </div>
-        }
-        -->
-    </span>
-
-    @foreach (var t in @Model.repo.GetTestItemsByCategory()
-                               .OrderBy(x => x.QuestionType)
-                               .ThenBy(x => x.QuestionCategory))
-    {
-        <div style="font-size:3rem;">@Html.Raw(t.IconAsHtml()) @t.ToString()</div>
-    }
-
-    <h1>Font Awesome Icons</h1>
-    <i class="fas fa-bicycle"></i>
-    <i class="fas fa-drum"></i>
-    <i class="fas fa-parking"></i>
-    <hr />
-    <hr />
-
-    <h1>Test Items:</h1>
-
-    @foreach (var t in @Model.repo.GetTestItemsForATest()
-                            .OrderBy(x => x.QuestionType)
-                            .ThenBy(x => x.QuestionCategory))
-    {
-        <div style="font-size:3rem;">@Html.Raw(t.IconAsHtml()) @t.ToString()</div>
-    }
-
- * 
- * 
- * 
- * 
- */
